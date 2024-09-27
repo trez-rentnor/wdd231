4b1da9e5eb37abfe1f3b7dcfad2206fdead214e9
Randy Jones <randy910@gmail.com>
2024-09-27 08:59:48 -0700

    Home Page header complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('#navigation');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
