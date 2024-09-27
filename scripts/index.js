82b7c73d5b6dcd5af1df390fc8b46542af442507
Randy Jones <randy910@gmail.com>
2024-09-27 12:55:48 -0700

    Complete Week 1 assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#navigation');
 
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+currentyear.innerHTML = today.getFullYear();
+lastModified.innerHTML = "Last Modification: " + document.lastModified;
